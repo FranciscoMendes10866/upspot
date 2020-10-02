d881b6e240aafb488b543659d8ae08dacc38210f
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-10-02 18:00:38 +0100

    boostrap

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <Navbar />
+    <div class="container mt-5">
+      <div class="row py-5">
+        <div class="col-sm">
+          <h1 class="text-left display-1 font-weight-bold">
+            Discover events for all the things you love.
+          </h1>
+          <button type="button" class="btn btn-primary btn-lg mt-5">
+            Join Meetup
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
