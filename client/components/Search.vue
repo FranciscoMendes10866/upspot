598dd32f8ec16b7ade014384e3f2e14bece4c3f9
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-10-02 22:34:04 +0100

    search component is responsive

--- client/components/Search.vue
+++ client/components/Search.vue
@@ -4,9 +4,8 @@
       <div class="col-12">
         <input
           type="text"
-          class="form-control"
+          class="form-control limit"
           id="exampleFormControlInput1"
-          style="width: 400px"
           placeholder="Encontre o seu próximo evento."
         />
       </div>
@@ -33,3 +32,14 @@
 import Vue from 'vue'
 export default Vue.extend({})
 </script>
+
+<style scoped>
+.limit {
+  width: 400px;
+}
+@media only screen and (max-width: 600px) {
+  .limit {
+    width: 300px;
+  }
+}
+</style>
