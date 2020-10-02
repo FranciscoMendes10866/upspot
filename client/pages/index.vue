f7ad9a9455326f6a9da65516e86f131221b8b504
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-10-02 21:35:35 +0100

    added box shadow

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,30 +1,36 @@
 <template>
   <div>
     <!-- Navbar -->
-    <Navbar />
+    <section>
+      <Navbar />
+    </section>
     <!-- Hero Landing -->
-    <div class="container mt-5">
-      <div class="row py-5">
-        <div class="col-sm">
-          <h1 class="text-left display-1 font-weight-bold">
-            Descubra eventos para todas as coisas que você ama.
-          </h1>
-          <button type="button" class="btn btn-primary btn-lg mt-5">
-            Começar.
-          </button>
-        </div>
-      </div>
-    </div>
-    <!-- Search Component -->
-    <div class="container d-flex justify-content-center mt-4">
-      <div class="row">
-        <div class="col">
-          <div class="card px-4 py-4">
-            <Search />
+    <section class="mt-5">
+      <div class="container">
+        <div class="row py-5">
+          <div class="col-sm">
+            <h1 class="text-left display-1 font-weight-bold">
+              Descubra eventos para todas as coisas que você ama.
+            </h1>
+            <button type="button" class="btn btn-primary btn-lg mt-4">
+              Começar.
+            </button>
           </div>
         </div>
       </div>
-    </div>
+    </section>
+    <!-- Search Component -->
+    <section class="mt-4">
+      <div class="container d-flex justify-content-center">
+        <div class="row">
+          <div class="col">
+            <div class="card px-4 py-4 shadow">
+              <Search />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -40,3 +46,11 @@ export default Vue.extend({
   },
 })
 </script>
+
+<style scoped>
+.shadow {
+  -webkit-box-shadow: 3px 3px 5px 6px #ccc; /* Safari 3-4, iOS 4.0.2 - 4.2, Android 2.3+ */
+  -moz-box-shadow: 3px 3px 5px 6px #ccc; /* Firefox 3.5 - 3.6 */
+  box-shadow: 3px 3px 5px 6px #ccc; /* Opera 10.5, IE 9, Firefox 4+, Chrome 6+, iOS 5 */
+}
+</style>
--- client/pages/register.vue
+++ client/pages/register.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="is-full-height">
+  <div class="is-full-height d-flex justify-content-center align-items-center">
     <div class="container limit">
       <div class="row">
         <div class="col">
-          <div class="card px-4 py-4">
+          <div class="card px-4 py-4 shadow">
             <h1 class="text-center text-muted">Criar conta</h1>
             <input
               class="mt-3 form-control"
@@ -34,13 +34,15 @@ export default Vue.extend({})
 
 <style scoped>
 .is-full-height {
-  display: flex;
   height: 100vh;
   width: 100vw;
-  justify-content: center;
-  align-items: center;
 }
 .limit {
   max-width: 400px;
+}
+.shadow {
+  -webkit-box-shadow: 3px 3px 5px 6px #ccc; /* Safari 3-4, iOS 4.0.2 - 4.2, Android 2.3+ */
+  -moz-box-shadow: 3px 3px 5px 6px #ccc; /* Firefox 3.5 - 3.6 */
+  box-shadow: 3px 3px 5px 6px #ccc; /* Opera 10.5, IE 9, Firefox 4+, Chrome 6+, iOS 5 */
 }
 </style>
